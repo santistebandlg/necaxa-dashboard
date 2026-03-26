6c8d40c6d98114de392b8c35cdf484346427f295
santistebandlg <santistebandlg@gmail.com>
2026-03-25 23:43:50 -0600

    Register Chart.js controllers

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from 'chart.js'
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
